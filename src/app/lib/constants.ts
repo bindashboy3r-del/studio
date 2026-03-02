a8fcb1d0f5024928e4419c12df37e55e56f33646
Chetan Kumar <bindashboy3r@gmail.com>
2026-03-02 20:10:15 +0000

    Comment minimum 50 rakh lo

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,11 +1,11 @@
-export type Platform = 'instagram';
-
 export interface SMMService {
   id: string;
   name: string;
   pricePer1000: number; // -1 indicates unavailable/Coming Soon
   minQuantity: number;
 }
+
+export type Platform = 'instagram';
 
 export const PLATFORMS: Record<Platform, string> = {
   instagram: 'Instagram',
@@ -16,7 +16,7 @@ export const SERVICES: Record<Platform, SMMService[]> = {
     { id: 'followers', name: 'Followers', pricePer1000: 89.0, minQuantity: 100 },
     { id: 'likes', name: 'Likes', pricePer1000: 18.0, minQuantity: 100 },
     { id: 'views', name: 'Views', pricePer1000: 0.60, minQuantity: 500 },
-    { id: 'comments', name: 'Comments', pricePer1000: 260.0, minQuantity: 10 },
+    { id: 'comments', name: 'Comments', pricePer1000: 260.0, minQuantity: 50 },
     { id: 'shares', name: 'Shares', pricePer1000: 7.0, minQuantity: 100 },
     { id: 'story_views', name: 'Story Views', pricePer1000: 65.0, minQuantity: 100 },
     { id: 'reel_views', name: 'Reel Views', pricePer1000: 0.56, minQuantity: 500 },
